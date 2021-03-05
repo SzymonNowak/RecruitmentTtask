6299e32a61aa21c8ebf7a336b5ad05b80393638a
SzymonNowak <nnja5@o2.pl>
2021-03-05 22:12:04 +0100

    add wraper to center sucess

--- front/src/views/MakeOrder.js
+++ front/src/views/MakeOrder.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import ContactForm from "components/Organisms/ContactForm/ContactForm";
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
 const MakeOrder = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const submitForm = () => {
     setIsSubmitted(true);
   };
-
   return (
     <div>
       {!isSubmitted ? (
         <ContactForm submitForm={submitForm} />
       ) : (
-        <div>succes</div>
+        <Wrapper>
+          <h1>Succes</h1>
+        </Wrapper>
       )}
     </div>
   );
